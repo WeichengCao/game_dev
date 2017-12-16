02e0c8ae075dbcb29320a1304029450562bd4966
WeichengCao <weicheng@localhost.localdomain>
2017-12-17 06:06:01 +0800

    计时器直接使用skynet.timeout

--- lualib/base/timer.lua
+++ lualib/base/timer.lua
@@ -1,39 +1,6 @@
 local skynet = require "skynet"
 local ltimer = require 'ltimer'
 
-local oTimerMgr = nil
-
-function get_time()
-    return math.floor(skynet.time()*1000)
-end
-
-CTimerMgr = {}
-CTimerMgr.__index = CTimerMgr
-
-function CTimerMgr:New()
-    local o = setmetatable({}, self)
-    o.m_iDispatchId = 0
-    ltimer.ltimer_create(get_time())
-    return o
-end
-
-function CTimerMgr:DispatchId()
-    self.m_iDispatchId = self.m_iDispatchId + 1
-    return self.m_iDispatchId
-end
-
-function CTimerMgr:AddTimeCb(func, iDelay)
-    ltimer.ltimer_add_time(func, iDelay)
-end
-
-function CTimerMgr:CheckUpdate()
-    ltimer.ltimer_update(get_time())
-    skynet.timeout(1, function()
-        self:CheckUpdate()
-    end)
-end
-
-
 CTimer = {}
 CTimer.__index = CTimer
 
@@ -41,12 +8,23 @@ function CTimer:New()
     local o = setmetatable({}, self)
     o.m_mKey2Index = {}
     o.m_mIndex2Func = {}
+    o.m_lReuseIndex = {}
+    o.m_iDispatchId = 0
     return o
 end
 
 function CTimer:Release()
     self.m_mKey2Index = {}
     self.m_mIndex2Func = {}
+    self.m_mReuseIndex = {}
+end
+
+function CTimer:DispatchId()
+    if #self.m_lReuseIndex > 0 then
+        return table.remove(self.m_lReuseIndex, #self.m_lReuseIndex)
+    end
+    self.m_iDispatchId = self.m_iDispatchId + 1
+    return self.m_iDispatchId
 end
 
 function CTimer:AddTimeCb(sKey, iDelay, func)
@@ -57,7 +35,7 @@ function CTimer:AddTimeCb(sKey, iDelay, func)
         self.m_mKey2Index[sKey] = nil
     end
 
-    local iCb = oTimerMgr:DispatchId()
+    local iCb = self:DispatchId()
     self.m_mKey2Index[sKey] = iCb
     self.m_mIndex2Func[iCb] = func
     
@@ -67,8 +45,9 @@ function CTimer:AddTimeCb(sKey, iDelay, func)
         end
         self.m_mKey2Index[sKey] = nil
         self.m_mIndex2Func[iCb] = nil
+        table.insert(self.m_lReuseIndex, iCb)
     end
-    oTimerMgr:AddTimeCb(func, iDelay)
+    skynet.timeout(iDelay, func)
 end
 
 function CTimer:DelTimeCb(sKey)
