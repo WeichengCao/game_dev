8f10e274549d7ba6fc6a6dd86ab907835bfe3efc
weicheng <weicheng@localhost.localdomain>
2018-01-31 20:37:18 +0800

    道具使用协议

--- service/world/netcmd/init.lua
+++ service/world/netcmd/init.lua
@@ -1,13 +1,24 @@
 local M = {}
 
 --M.test = import(service_path("netcmd.test"))
+M.item = import(service_path("netcmd.item"))
 
 function M.Invoke(sMod, sMsg, fd, mData)
     if not M[sMod] then
         skynet.error("uninit net module:"..sMod)
         return
     end
-    M[sMod][sMsg](fd, mData)
+
+    local func = M[sMod][sMsg]
+    if not func then
+        skynet.error("uninit net func:"..sMod.."->"..sMsg)
+        return
+    end
+
+    local oPlayer = global.oWorldMgr:GetOnlinePlayerByFd(fd)
+    if oPlayer then
+        safe_call(func, oPlayer, mData)
+    end
 end
 
 return M
